9774ebc10f4dcf82261a2ce255d6861b979f7bfb
piyushkumar6844 <162744118+piyushkumar6844@users.noreply.github.com>
2024-07-24 17:09:34 +0530

    Update App.js
    Just trying to erase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-//import About from './About';
 import Login from './components/Login';
 import Home from './components/Home';
 import Signup from './components/Signup';
 import HomeworkState from './context/HomeworkState';
-//import Teachernote from './components/Submissions'; 
-//import Home from './components/Home';
-// /import User from './components/User';
 
 function App() {
   return (
@@ -17,13 +13,13 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-        {/* <Route exact path="/home/teachernote" element={<Teachernote />} /> */}
+        
           
           <Route exact path="/home" element={<Home />} />
-          {/* <Route exact path="/about" element={<About />} /> */}
+          
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<Signup />} />
-         {/* < Route exact path="/user" element={<User />} /> */}
+         
         </Routes>
       </Router>
     </HomeworkState>
